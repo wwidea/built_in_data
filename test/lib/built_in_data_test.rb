833541e1a635d8f41485c461fe2f0601d06c9309
Aaron Baldwin <baldwina@brightwayslearning.org>
2023-02-06 08:50:08 -0700

    rubocop updates

--- test/lib/built_in_data_test.rb
+++ test/lib/built_in_data_test.rb
@@ -5,18 +5,18 @@ require "test_helper"
 class BuiltInDataTest < ActiveSupport::TestCase
   HASH_DATA = {
     test: {
-      name:         "Yellowstone National Park",
-      established:  "1872-03-01",
-      url:          "http://www.nps.gov/yell/index.htm",
+      name:        "Yellowstone National Park",
+      established: "1872-03-01",
+      url:         "http://www.nps.gov/yell/index.htm"
     }
-  }
+  }.freeze
 
   test "should return true for built_in?" do
-    assert_equal true, NationalPark.new(built_in_key: "test").built_in?
+    assert_predicate NationalPark.new(built_in_key: "test"), :built_in?
   end
 
   test "should return false for built_in?" do
-    assert_equal false, NationalPark.new(built_in_key: "").built_in?
+    assert_not_predicate NationalPark.new(built_in_key: ""), :built_in?
   end
 
   test "should load built in data" do
@@ -24,7 +24,7 @@ class BuiltInDataTest < ActiveSupport::TestCase
       load_hash_data
     end
 
-    assert_equal "Yellowstone National Park", NationalPark.find_by_built_in_key("test").name
+    assert_equal "Yellowstone National Park", yellowstone.name
   end
 
   test "should remove built in data" do
@@ -34,11 +34,11 @@ class BuiltInDataTest < ActiveSupport::TestCase
       NationalPark.load_built_in_data!({})
     end
 
-    assert_nil NationalPark.find_by_built_in_key("test")
+    assert_nil yellowstone
   end
 
   test "should not remove a record without a built_in_key" do
-    park = NationalPark.create(:name => "Testing")
+    park = NationalPark.create(name: "Testing")
 
     assert_difference "NationalPark.count", 1 do
       load_hash_data
@@ -48,11 +48,13 @@ class BuiltInDataTest < ActiveSupport::TestCase
 
   test "should update existing built in data" do
     load_hash_data
-    assert_not_equal "http://en.wikipedia.org/wiki/Yellowstone_National_Park", NationalPark.find_by_built_in_key("test").url
+
+    assert_not_equal "http://en.wikipedia.org/wiki/Yellowstone_National_Park", yellowstone.url
     modified_hash = HASH_DATA.dup
     modified_hash[:test][:url] = "http://en.wikipedia.org/wiki/Yellowstone_National_Park"
     NationalPark.load_built_in_data!(modified_hash)
-    assert_equal "http://en.wikipedia.org/wiki/Yellowstone_National_Park", NationalPark.find_by_built_in_key("test").url
+
+    assert_equal "http://en.wikipedia.org/wiki/Yellowstone_National_Park", yellowstone.url
   end
 
   test "should load built in data from yaml file" do
@@ -75,26 +77,29 @@ class BuiltInDataTest < ActiveSupport::TestCase
 
   test "should return active record objects from load" do
     parks = NationalPark.load_built_in_data!
-    assert_equal(2,parks.size)
-    assert_equal(NationalPark,parks.first.class)
-    assert_equal(false,parks.first.new_record?)
+
+    assert_equal  2,            parks.size
+    assert_equal  NationalPark, parks.first.class
+    assert_not    parks.first.new_record?
   end
 
   test "should load yaml defaults" do
     NationalPark.load_built_in_data!
-    assert_equal true, NationalPark.find_by(name: "Yellowstone National Park").active
+
+    assert NationalPark.find_by(name: "Yellowstone National Park").active
   end
 
   test "should return built_in_data object database id" do
     load_hash_data
+
     assert_equal NationalPark.where(name: "Yellowstone National Park").first.id, NationalPark.built_in_object_id(:test)
   end
 
   test "should clear built_in_object_ids cache when delete_all is called" do
-    NationalPark.instance_variable_set("@built_in_object_ids", "testing")
-
+    NationalPark.instance_variable_set(:@built_in_object_ids, "testing")
     NationalPark.delete_all
-    assert_nil NationalPark.instance_variable_get("@built_in_object_ids")
+
+    assert_nil NationalPark.instance_variable_get(:@built_in_object_ids)
   end
 
   private
@@ -103,4 +108,7 @@ class BuiltInDataTest < ActiveSupport::TestCase
     NationalPark.load_built_in_data!(HASH_DATA)
   end
 
+  def yellowstone
+    NationalPark.find_by(built_in_key: "test")
+  end
 end
